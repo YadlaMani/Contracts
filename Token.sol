59295461e67a1a150e4234e2cbedfe1064b186b5
Mani Yadla <133841094+YadlaMani@users.noreply.github.com>
2024-12-19 21:20:10 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 contract Token{
     address public owner;
     mapping(address=>uint)balances;
+    mapping(address=>mapping(address=>uint))public allowances;
     uint public totalSupply=0;
     constructor(){
         owner=msg.sender;
@@ -43,5 +44,21 @@ contract Token{
     function burn(uint _amount) public{
         require(balances[msg.sender]>=_amount,"No sufficient balance");
         balances[msg.sender]-=_amount;
+    }
+    //allowance
+    function approve(address _spender,uint _amount)public {
+        require(balances[msg.sender]>=_amount,"Insufficient funds");
+        allowances[msg.sender][_spender]=_amount;
+
+    }
+    //transferFrom
+    function transferFrom(address _from,address _to,uint _amount)public{
+        require(allowances[_from][msg.sender]>=_amount);
+        require(balances[_from]>=_amount,"Sender doesn't have sufficient funds");
+        allowances[_from][msg.sender]-=_amount;
+        balances[_from]-=_amount;
+        balances[_to]+=_amount;
+
+
     }
 }
