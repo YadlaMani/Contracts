d6be6911464ae015cbc713b69502606818937c29
Mani Yadla <133841094+YadlaMani@users.noreply.github.com>
2025-01-19 17:17:57 +0530

    Update DelegateCall.sol

--- DelegateCall.sol
+++ DelegateCall.sol
@@ -1,1 +1,23 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity ^0.8.0;
+//proxy contract
+contract Storage{
+    uint public num;
+    address implementation;
+    constructor(address _implementation){
+        num=0;
+        implementation=_implementation;
+    }
+    function setNum(uint _num) public{
+        (bool sucess,)=implementation.delegatecall(abi.encodeWithSignature("setNum(uint256)", _num));
+        require(sucess,"Error while delegating call");
+    }
+}
+//logic contract
+contract Implementation{
+    uint public num;
+    function setNum(uint _num)public{
+        num=_num;
 
+    }
+}
