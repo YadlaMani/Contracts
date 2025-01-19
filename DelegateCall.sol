93d3242bc68c480e2eec367594ac120cf21214e1
Mani Yadla <133841094+YadlaMani@users.noreply.github.com>
2025-01-19 17:51:44 +0530

    Update DelegateCall.sol

--- DelegateCall.sol
+++ DelegateCall.sol
@@ -1,10 +1,12 @@
 //SPDX-License-Identifier:GPL-3.0
 pragma solidity ^0.8.0;
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 //proxy contract
-contract Storage{
-    uint public num;
-    address implementation;
-    constructor(address _implementation){
+contract Storage is Ownable{
+    //address owner (storage slot1)
+    uint public num;//slot2
+    address implementation;//slot3
+    constructor(address _implementation)Ownable(msg.sender){
         num=0;
         implementation=_implementation;
     }
@@ -12,13 +14,14 @@ contract Storage{
         (bool sucess,)=implementation.delegatecall(abi.encodeWithSignature("setNum(uint256)", _num));
         require(sucess,"Error while delegating call");
     }
-    function setImplementation(address _implementation)public{
+    function setImplementation(address _implementation)public onlyOwner{
         implementation=_implementation;
     }
 }
-//logic contract
+//logic contracts
 contract Implementation1{
-    uint public num;
+    address public owner;//storage slot0(dummy)
+    uint public num;//slot1
     function setNum(uint _num)public{
         num=_num;
 
